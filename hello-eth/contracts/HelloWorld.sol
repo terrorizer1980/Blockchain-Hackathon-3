70baa52e9fdaab9ee2f9f786a1078e1e70e42b7e
FlamboD <FlamboD2000@gmail.com>
2021-08-03 12:27:41 +0200

    Completed S2[3-8]

--- hello-eth/contracts/HelloWorld.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
-
-contract HelloWorld {
-    function sayHello() public pure returns (string memory) {
-        return 'Hello World!';
-    }
-}
